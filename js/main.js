a77221920ca3aa8fbcff79bc8334145aaef07657
eric-k-chu <kiyuen88ec@gmail.com>
2023-10-02 05:00:28 +0000

    Created insertStats()

--- js/main.js
+++ js/main.js
@@ -16,6 +16,10 @@ const $accountInfoName = document.querySelector('#account-info-name');
 const $accountInfoFollowers = document.querySelector('#account-info-followers');
 const $accountInfoLocation = document.querySelector('#account-info-location');
 const $accountInfoJoined = document.querySelector('#account-info-joined');
+const $accountInfoLeague = document.querySelector('#account-info-league');
+
+// Account Stats
+const $accountStats = document.querySelector('#account-stats');
 
 const months = [
   'January',
@@ -38,14 +42,14 @@ $mainSearch.addEventListener('keydown', getPlayerInfo);
 function getPlayerInfo(event) {
   if (event.key === 'Enter') {
     const xhr = new XMLHttpRequest();
-    const endpoint = `https://api.chess.com/pub/player/${$headerSearch.value}`;
-    xhr.open('GET', endpoint);
+    xhr.open('GET', `https://api.chess.com/pub/player/${$headerSearch.value}`);
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
 
       if (xhr.status === 200) {
         data.viewSwap($playerInfo);
         insertAccountInfo(xhr.response);
+        insertStats($headerSearch.value);
       } else {
         $errorMsg.textContent = 'Unable to find ' + $headerSearch.value + '.';
         data.viewSwap($failedSearch);
@@ -78,4 +82,130 @@ function insertAccountInfo(response) {
   const year = date.getFullYear();
   const joinedDate = ` Joined ${month} ${year}`;
   $accountInfoJoined.textContent = joinedDate;
+
+  $accountInfoLeague.textContent = response.league;
+}
+
+/* <div class="stats-box">
+    <div class="mode-icon">
+      <i class="fa-solid fa-sun"></i>
+    </div>
+    <div class="mode-info">
+      <p class="mode-name">Daily</p>
+      <p class="mode-rating">1526</p>
+    </div>
+    <div class="mode-wpct">
+      <p class="mode-pct">70.46%</p>
+      <p class="mode-desc">Win %</p>
+    </div>
+  </div> */
+
+function renderStat(type, stats) {
+  const game = getGame(type);
+
+  const $parentDiv = document.createElement('div');
+  $parentDiv.className = 'stats-box';
+
+  const $iconDiv = document.createElement('div');
+  $iconDiv.className = 'mode-icon';
+  const $icon = document.createElement('i');
+  $icon.className = game.icon;
+
+  const $infoDiv = document.createElement('div');
+  $infoDiv.className = 'mode-info';
+  const $pName = document.createElement('p');
+  $pName.className = 'mode-name';
+  $pName.textContent = game.name;
+  const $pRating = document.createElement('p');
+  $pRating.className = 'mode-rating';
+
+  const $wpctDiv = document.createElement('div');
+  $wpctDiv.className = 'mode-wpct';
+  const $pPCT = document.createElement('p');
+  $pPCT.className = 'mode-pct';
+  const $pDesc = document.createElement('p');
+  $pDesc.className = 'mode-desc';
+  $pDesc.textContent = 'Win %';
+
+  if (game.name === 'Puzzles') {
+    $pRating.textContent = stats.lowest.rating;
+    $pPCT.textContent = stats.highest.rating;
+    $pDesc.textContent = 'Highest';
+  } else if (game.name === 'Puzzle Rush') {
+    $pPCT.textContent = stats.best.total_attempts;
+    $pPCT.textContent = stats.best.score;
+    $pDesc.textContent = 'Score';
+  } else {
+    $pRating.textContent = stats.last.rating;
+    $pPCT.textContent = getWPCT(stats.record.win, stats.record.loss, stats.record.draw);
+  }
+
+  $parentDiv.appendChild($iconDiv);
+  $parentDiv.appendChild($infoDiv);
+  $parentDiv.appendChild($wpctDiv);
+
+  $iconDiv.appendChild($icon);
+  $infoDiv.appendChild($pName);
+  $infoDiv.appendChild($pRating);
+  $wpctDiv.appendChild($pPCT);
+  $wpctDiv.appendChild($pDesc);
+
+  return $parentDiv;
+}
+
+function insertStats(username) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `https://api.chess.com/pub/player/${username}/stats`);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function (event) {
+    const gameModes = Object.keys(xhr.response);
+    gameModes.forEach((key, index) => {
+      if (key !== 'fide') {
+        const $statBox = renderStat(key, xhr.response[key]);
+        $accountStats.appendChild($statBox);
+      }
+    });
+  });
+  xhr.send();
+}
+
+function getGame(string) {
+  const obj = {};
+  switch (string) {
+    case 'chess_daily':
+      obj.name = 'Daily';
+      obj.icon = 'fa-solid fa-sun';
+      return obj;
+    case 'chess960_daily':
+      obj.name = 'Daily 960';
+      obj.icon = 'fa-regular fa-sun';
+      return obj;
+    case 'chess_rapid':
+      obj.name = 'Rapid';
+      obj.icon = 'fa-solid fa-stopwatch';
+      return obj;
+    case 'chess_bullet':
+      obj.name = 'Bullet';
+      obj.icon = 'fa-solid fa-rocket';
+      return obj;
+    case 'chess_blitz':
+      obj.name = 'Blitz';
+      obj.icon = 'fa-solid fa-bolt';
+      return obj;
+    case 'tactics':
+      obj.name = 'Puzzles';
+      obj.icon = 'fa-solid fa-puzzle-piece';
+      return obj;
+    case 'puzzle_rush':
+      obj.name = 'Puzzle Rush';
+      obj.icon = 'fa-solid fa-bolt-lightning';
+      return obj;
+  }
+}
+
+function getWPCT(win, loss, draw) {
+  const total = win + loss + draw;
+  const dec = (2 * win + draw) / (2 * total);
+  const pct = Math.trunc(dec * 100);
+  return `${pct}%`;
 }
